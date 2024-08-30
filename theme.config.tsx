e8b2d0ad301927bec933d173c252537cbde9cab7
Jingxiang Mo <jingxiangmo@gmail.com>
2024-08-30 09:53:44 -0700

    update meta data

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
   logo: <span>K-Scale Labs Docs</span>,
@@ -17,6 +19,22 @@ const config: DocsThemeConfig = {
   footer: {
     text: "K-Scale Labs Documentation",
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      'https://docs.kscale.dev' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={'K-Scale Labs Docs'} />
+        <meta property="og:description" content={'K-Scale open source humanoid building guide'} />
+      </>
+    );
+  }
 };
 
 export default config;
